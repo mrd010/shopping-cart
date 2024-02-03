7e0090ae57b50e954ca650ce8307b3af8fb41f58
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-02-03 21:10:14 +0330

    Create default routes

---
+++ src/Router.jsx
@@ -0,0 +1,29 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import Layout from './routes/Layout';
+import Home from './routes/Home';
+import Shop from './routes/Shop';
+import Cart from './routes/Cart';
+import Item from './routes/Item';
+import ErrorPage from './ErrorPage';
+
+const Router = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout></Layout>} errorElement={<ErrorPage></ErrorPage>}>
+        <Route element={<Home></Home>} index></Route>
+        <Route element={<Home></Home>} path="/home"></Route>
+        <Route element={<Shop></Shop>} path="/shop">
+          <Route element={<Item></Item>} path="/:itemId"></Route>
+        </Route>
+        <Route element={<Cart></Cart>} path="/cart"></Route>
+      </Route>
+    )
+  );
+  return <RouterProvider router={router}></RouterProvider>;
+};
+export default Router;
